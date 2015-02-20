5a5cb4239235cb17efa56538d6cd986dc9fce92f
Shunchao Wang <wang.2799@osu.edu>
2015-02-20 16:04:06 -0500

    Finished up MerchantStatusDao unit testing.

--- code/smartpay/core/src/test/java/com/lambo/smartpay/test/dao/MerchantStatusDaoTest.java
+++ code/smartpay/core/src/test/java/com/lambo/smartpay/test/dao/MerchantStatusDaoTest.java
@@ -16,8 +16,10 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotEquals;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
 
 /**
  * Created by swang on 2/19/2015.
@@ -35,20 +37,36 @@ public class MerchantStatusDaoTest {
 
     @Test
     @Transactional
-    public void testCreate() {
+    public void testCrud() {
 
+        // create new merchant status
         MerchantStatus merchantStatus = new MerchantStatus();
         merchantStatus.setName("name");
         merchantStatus.setDescription("Description");
-        MerchantStatus status = merchantStatusDao.create(merchantStatus);
+        merchantStatus = merchantStatusDao.create(merchantStatus);
 
-        LOG.debug("Status Id: " + status.getId());
+        assertNotNull(merchantStatus);
+        assertEquals("name", merchantStatus.getName());
 
+        // read the newly created merchant status
+        merchantStatus = merchantStatusDao.get(merchantStatus.getId());
+        assertNotNull(merchantStatus);
 
-        assertNotNull(status);
+        // update the name
+        merchantStatus.setName("updated name");
+        merchantStatus = merchantStatusDao.update(merchantStatus);
+        assertNotNull(merchantStatus);
+        assertEquals("updated name", merchantStatus.getName());
 
-        List<MerchantStatus> statuses = merchantStatusDao.getAll();
-        LOG.debug("Status Count: " + statuses.size());
-        assertNotEquals(statuses.size(), 0);
+        // delete
+        merchantStatusDao.delete(merchantStatus.getId());
+        assertNull(merchantStatusDao.get(merchantStatus.getId()));
+
+    }
+
+    @Test
+    public void testGetAll() {
+        List<MerchantStatus> merchantStatuses = merchantStatusDao.getAll();
+        assertNotNull(merchantStatuses);
     }
 }
