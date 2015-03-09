6bd1b3ecc6a559dca745af3a12ee55ea02887124
Shunchao Wang <wang.2799@osu.edu>
2015-03-09 09:54:31 -0400

    Finished shipment dao.

---
+++ code/smartpay/core/src/main/java/com/lambo/smartpay/persistence/dao/ShipmentDao.java
@@ -0,0 +1,9 @@
+package com.lambo.smartpay.persistence.dao;
+
+import com.lambo.smartpay.persistence.entity.Shipment;
+
+/**
+ * Created by swang on 3/9/2015.
+ */
+public interface ShipmentDao extends GenericDateQueryDao<Shipment, Long> {
+}
