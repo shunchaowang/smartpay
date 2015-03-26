0fc067bff9b20922631bca2551939f76f3b5fcd7
Shunchao Wang <wang.2799@osu.edu>
2015-03-26 12:58:14 -0400

    Working on ecs and pay.

---
+++ code/smartpay/ecs/src/main/java/com/lambo/smartpay/ecs/web/controller/SiteController.java
@@ -0,0 +1,7 @@
+package com.lambo.smartpay.ecs.web.controller;
+
+/**
+ * Created by swang on 3/26/2015.
+ */
+public class SiteController {
+}
---
+++ code/smartpay/pay/src/main/java/com/lambo/smartpay/pay/web/controller/OrderController.java
@@ -0,0 +1,7 @@
+package com.lambo.smartpay.pay.web.controller;
+
+/**
+ * Created by swang on 3/26/2015.
+ */
+public class OrderController {
+}
